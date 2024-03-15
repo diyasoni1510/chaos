f23b0c9fb01d11c8fc367e31cf332369aa522b74
divyanjali soni <divyanjali@360adaptive.com>
2024-03-15 15:43:08 +0530

    commit

---
+++ src/app/api/posts/getpostbypostid/route.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import connect from "@/dbConfig/dbConfig";
+import Post from "@/models/postModel";
+import { NextResponse, NextRequest } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const { postId } = await request.json();
+    const allPosts = await Post.findOne({ _id: postId });
+    if (allPosts) {
+      const savepostinfo = await Post.aggregate([
+        {
+          $match: { _id: allPosts._id }, // Match the post by its _id
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "username",
+            foreignField: "username",
+            as: "userDetails",
+          },
+        },
+        {
+          $sort: { createdAt: -1 }, // Newest posts first based on 'createdAt'
+        },
+      ]);
+      console.log(savepostinfo);
+      const response = NextResponse.json({
+        message: "all posts",
+        data: savepostinfo,
+      });
+      return response;
+    } else {
+      console.log("Post not found");
+      const response = NextResponse.json({
+        message: "erorrs",
+      });
+      return response;
+    }
+
+    // return NextResponse.json({message:"all posts",data:allPosts})
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
--- src/app/components/savedPosts/savedPosts.jsx
+++ src/app/components/savedPosts/savedPosts.jsx
@@ -5,16 +5,25 @@ import { IoIosArrowBack } from "react-icons/io";
 
 const SavedPosts = () => {
   const { updateCurrentPage } = useRouteContext();
-  const [savedPostsImage,setSavedPostsImage] = useState([])
+  const [savedPostsImage, setSavedPostsImage] = useState([]);
+  const [showBig, setShowBig] = useState("");
   const getAllSavedPosts = async () => {
     const response = await axios.post("/api/posts/getsaveposts", {
       userId: JSON.parse(localStorage?.getItem("LoggedInUser"))._id,
     });
-    console.log(response)
+    response.data.data.map(async (post) => {
+      const getpostdetails = await axios.post("/api/posts/getpostbypostid", {
+        postId: post.postId,
+      });
+      setSavedPostsImage((prev) => [...prev, getpostdetails.data.data]);
+    });
   };
-  useEffect(()=>{
-    getAllSavedPosts()
-  },[])
+  useEffect(() => {
+    getAllSavedPosts();
+  }, []);
+  useEffect(() => {
+    console.log(savedPostsImage);
+  }, [savedPostsImage]);
   return (
     <>
       <div>
@@ -28,15 +37,35 @@ const SavedPosts = () => {
           <p>Saved Posts</p>
         </div>
         <div className="w-100 flex flex-wrap pt-2">
-          <div className="w-1/3 p-1 h-[110px]">
-            <div className="bg-gray-200 w-full h-full"></div>
-          </div>
-          <div className="w-1/3 p-1 h-[110px]">
-            <div className="bg-gray-200 w-full h-full"></div>
-          </div>
-          <div className="w-1/3 p-1 h-[110px]">
-            <div className="bg-gray-200 w-full h-full"></div>
-          </div>
+          {savedPostsImage.length > 1 ? (
+            savedPostsImage.map((post) => {
+              console.log(post);
+              return (
+                <>
+                  <div
+                    className="w-1/3 p-1 h-[110px]"
+                    key={post[0]._id}
+                    onClick={() => setShowBig(post[0]._id)}
+                  >
+                    <img src={post[0].post} className="h-full w-full" />
+                  </div>
+                  {showBig === post[0]._id && (
+                    <div>
+                      <img src={post[0].post} className="h-full w-full" />
+                    </div>
+                  )}
+                </>
+              );
+            })
+          ) : (
+            <>
+              {[...Array(9)].map((_, index) => (
+                <div className="w-1/3 p-1 h-[110px]" key={index}>
+                  <div className="bg-gray-200 w-full h-full"></div>
+                </div>
+              ))}
+            </>
+          )}
         </div>
       </div>
     </>
