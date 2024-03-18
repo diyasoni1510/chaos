3c04784fce6edf5ca1c89dc2573644c2dab7e7e7
divyanjali soni <divyanjali@360adaptive.com>
2024-03-18 11:31:58 +0530

    commit

--- src/app/components/reelsection.jsx
+++ src/app/components/reelsection.jsx
@@ -1,19 +1,20 @@
-"use client"
-import ProfileFooter from './profile/profilefooter/page'
+"use client";
+import ProfileFooter from "./profile/profilefooter/page";
 
 const reelsection = () => {
   return (
     <>
-    <div className='h-[670px] border border-black'>
+      <div className="h-screen flex justify-center items-center">
+        <p>In progress...</p>
         {/* <video autoPlay src="/reel.mp4" className='w-full h-full'></video> */}
-        <video autoPlay muted width="600">
-        <source src="/reel.mp4" type="video/mp4"/>
-        Your browser does not support the video tag.
-    </video>
-    </div>
-    <ProfileFooter/>
+        {/* <video autoPlay loop muted width="600">
+          <source src="/reel.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video> */}
+      </div>
+      <ProfileFooter />
     </>
-  )
-}
+  );
+};
 
-export default reelsection
+export default reelsection;
