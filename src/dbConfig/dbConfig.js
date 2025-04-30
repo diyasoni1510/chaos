198b7cfeb11143de1737af58a421d131369ac4ba
diyasoni1510 <sonidiya126@gmail.com>
2025-04-30 23:03:40 +0530

    update db

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -1,22 +1,21 @@
 import mongoose, { connection } from "mongoose";
 
+export default function connect() {
+  console.log("its goin in here");
+  try {
+    mongoose.connect(process.env.MONGO_URL);
 
-export default function connect (){
-    try {
-        mongoose.connect(process.env.MONGO_URL);
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("mongodb connected ");
+    });
 
-        const connection = mongoose.connection
-        connection.on('connected',()=>{
-            console.log("mongodb connected ")
-        })
-
-        connection.on('error',(error)=>{
-            console.log("mongoose connection failed" + error )
-            process.exit()
-        })
-        
-    } catch (error) {
-        console.log("something went wrong")
-        console.log(error)
-    }
+    connection.on("error", (error) => {
+      console.log("mongoose connection failed" + error);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("something went wrong");
+    console.log(error);
+  }
 }
